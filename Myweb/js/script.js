c770113a03e8ad074285458c4f7cb76f25501398
abc <prajapatijimil@yahoo.com>
2020-04-30 13:12:48 +0530

    donee

--- Myweb/js/script.js
+++ Myweb/js/script.js
@@ -1,8 +1,10 @@
-$(function() {
-	$("#navbar-toggle").blur(function(event) {
-		var sw = window.innerWidth;
-		if (sw < 768) {
-			$("#collapsable-nav").collapse('hide');
-		}
-	});
+$(function () { // Same as document.addEventListener("DOMContentLoaded"...
+
+  // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
+  $("#navbar-toggle").blur(function (event) {
+    var screenWidth = window.innerWidth;
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
+    }
+  });
 });
